e0d6a3156f80be40f947339ca58598daed0981d6
Harsh Desai <harsh@portworx.com>
2017-12-28 14:53:21 -0800

    Add px cluster provider implementation

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -12,9 +12,11 @@ import (
 	samplescheme "github.com/harsh-px/px-operator/pkg/client/clientset/versioned/scheme"
 	informers "github.com/harsh-px/px-operator/pkg/client/informers/externalversions"
 	listers "github.com/harsh-px/px-operator/pkg/client/listers/portworx.com/v1alpha1"
+	"github.com/harsh-px/px-operator/pkg/cluster"
+	// blank import to ensure px cluster provider gets registered
+	_ "github.com/harsh-px/px-operator/pkg/cluster/px"
 	"github.com/harsh-px/px-operator/pkg/crd"
 	"github.com/sirupsen/logrus"
-	appsv1beta2 "k8s.io/api/apps/v1beta2"
 	"k8s.io/api/core/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -22,6 +24,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	kwatch "k8s.io/apimachinery/pkg/watch"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -56,9 +59,9 @@ type Controller struct {
 
 	apiExtClientset apiextensionsclient.Interface
 
-	clustersLister listers.ClusterLister
-	clustersSynced cache.InformerSynced
-
+	clustersLister  listers.ClusterLister
+	clustersSynced  cache.InformerSynced
+	clusterProvider cluster.Cluster
 	// workqueue is a rate limited work queue. This is used to queue work to be
 	// processed instead of performing it as soon as a change happens. This
 	// means we can ensure we only process a fixed amount of resources at a
@@ -69,6 +72,11 @@ type Controller struct {
 	// Kubernetes API.
 	recorder  record.EventRecorder
 	resources []crd.CustomResource
+}
+
+type event struct {
+	key       string
+	eventType kwatch.EventType
 }
 
 // New returns a new controller for managing portworx clusters
@@ -83,7 +91,7 @@ func New(
 	pxInformer := pxoperatorInformerFactory.Portworx().V1alpha1().Clusters()
 
 	// Add portworx types to the default Kubernetes Scheme so Events can be
-	// logged for sample-controller types.
+	// logged for them
 	samplescheme.AddToScheme(scheme.Scheme)
 
 	resources := []crd.CustomResource{
@@ -97,18 +105,26 @@ func New(
 		},
 	}
 
+	clusterProvider, err := cluster.Get(portworx.GroupName, nil)
+	if err != nil {
+		logrus.Fatalf("failed to fetch cluster provider. Err: %v", err)
+	}
+
 	controller := &Controller{
 		kubeclientset:       kubeclientset,
 		pxoperatorclientset: pxoperatorclientset,
 		apiExtClientset:     apiExtClientset,
 		clustersLister:      pxInformer.Lister(),
 		clustersSynced:      pxInformer.Informer().HasSynced,
-		workqueue:           workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Clusters"),
-		recorder:            CreateRecorder(kubeclientset, controllerAgentName, ""),
-		resources:           resources,
+		clusterProvider:     clusterProvider,
+		workqueue: workqueue.NewNamedRateLimitingQueue(
+			workqueue.DefaultControllerRateLimiter(), "Clusters"),
+		recorder:  CreateRecorder(kubeclientset, controllerAgentName, ""),
+		resources: resources,
 	}
 
 	logrus.Info("Setting up event handlers")
+
 	// Set up an event handler for when Cluster resources change
 	pxInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    controller.onAddEvent,
@@ -121,24 +137,67 @@ func New(
 
 func (c *Controller) onAddEvent(obj interface{}) {
 	logrus.Infof("[debug] On add event: %v", obj)
-	c.enqueueCluster(obj)
+	c.handleObject(obj, kwatch.Added)
 }
 
 func (c *Controller) onUpdateEvent(oldObj, newObj interface{}) {
 	logrus.Infof("[debug] On update event: %v", newObj)
-	c.enqueueCluster(newObj)
+	c.handleObject(newObj, kwatch.Modified)
 }
 
 func (c *Controller) onDeleteEvent(obj interface{}) {
 	logrus.Infof("[debug] On delete event: %v", obj)
+	c.handleObject(obj, kwatch.Deleted)
 }
 
-// CreateRecorder creates a event recorder
-func CreateRecorder(kubecli kubernetes.Interface, name, namespace string) record.EventRecorder {
-	eventBroadcaster := record.NewBroadcaster()
-	eventBroadcaster.StartLogging(logrus.Infof)
-	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(kubecli.Core().RESTClient()).Events(namespace)})
-	return eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: name})
+// handleObject will take any resource implementing metav1.Object and attempt
+// to find the Cluster resource that 'owns' it. It does this by looking at the
+// objects metadata.ownerReferences field for an appropriate OwnerReference.
+// It then enqueues that Cluster resource to be processed. If the object does not
+// have an appropriate OwnerReference, it will simply be skipped.
+func (c *Controller) handleObject(obj interface{}, eventType kwatch.EventType) {
+	var object metav1.Object
+	var ok bool
+	if object, ok = obj.(metav1.Object); !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("error decoding object, invalid type"))
+			return
+		}
+		object, ok = tombstone.Obj.(metav1.Object)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
+			return
+		}
+		logrus.Infof("Recovered deleted object '%s' from tombstone", object.GetName())
+	}
+
+	logrus.Infof("Processing object: %s", object.GetName())
+	cluster, err := c.clustersLister.Clusters(object.GetNamespace()).Get(object.GetName())
+	if err != nil {
+		logrus.Infof("ignoring orphaned object '%s' of cluster '%s'", object.GetSelfLink(), object.GetName())
+		return
+	}
+
+	c.enqueueCluster(cluster, eventType)
+	return
+}
+
+// enqueueCluster takes a Cluster resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Cluster.
+func (c *Controller) enqueueCluster(obj interface{}, eventType kwatch.EventType) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	c.workqueue.AddRateLimited(event{
+		key:       key,
+		eventType: eventType,
+	})
 }
 
 // Run will set up the event handlers for types we are interested in, as well
@@ -192,7 +251,7 @@ func (c *Controller) runWorker() {
 }
 
 // processNextWorkItem will read a single work item off the workqueue and
-// attempt to process it, by calling the syncHandler.
+// attempt to process it, by calling the sync.
 func (c *Controller) processNextWorkItem() bool {
 	obj, shutdown := c.workqueue.Get()
 
@@ -209,30 +268,31 @@ func (c *Controller) processNextWorkItem() bool {
 		// put back on the workqueue and attempted again after a back-off
 		// period.
 		defer c.workqueue.Done(obj)
-		var key string
+		var ev event
 		var ok bool
-		// We expect strings to come off the workqueue. These are of the
-		// form namespace/name. We do this as the delayed nature of the
+		// We expect 'event' to come off the workqueue. The key in these events
+		// are of the form namespace/name. We do this as the delayed nature of the
 		// workqueue means the items in the informer cache may actually be
 		// more up to date that when the item was initially put onto the
 		// workqueue.
-		if key, ok = obj.(string); !ok {
+		if ev, ok = obj.(event); !ok {
 			// As the item in the workqueue is actually invalid, we call
 			// Forget here else we'd go into a loop of attempting to
 			// process a work item that is invalid.
 			c.workqueue.Forget(obj)
-			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			runtime.HandleError(fmt.Errorf("expected type event in workqueue but got %#v", obj))
 			return nil
 		}
-		// Run the syncHandler, passing it the namespace/name string of the
+
+		// Run the sync, passing it the namespace/name string of the
 		// custom resource (e.g PX cluster) to be synced.
-		if err := c.syncHandler(key); err != nil {
-			return fmt.Errorf("error syncing '%s': %s", key, err.Error())
+		if err := c.sync(ev); err != nil {
+			return fmt.Errorf("error syncing '%v': %s", ev, err.Error())
 		}
 		// Finally, if no error occurs we Forget this item so it does not
 		// get queued again until another change happens.
 		c.workqueue.Forget(obj)
-		logrus.Infof("Successfully synced '%s'", key)
+		logrus.Infof("Successfully synced '%v'", ev)
 		return nil
 	}(obj)
 
@@ -244,59 +304,51 @@ func (c *Controller) processNextWorkItem() bool {
 	return true
 }
 
-// syncHandler compares the actual state with the desired, and attempts to
+// sync compares the actual state with the desired, and attempts to
 // converge the two. It then updates the Status block of the Cluster resource
 // with the current status of the resource.
-func (c *Controller) syncHandler(key string) error {
+func (c *Controller) sync(ev event) error {
 	// Convert the namespace/name string into a distinct namespace and name
-	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	namespace, name, err := cache.SplitMetaNamespaceKey(ev.key)
 	if err != nil {
-		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", ev.key))
 		return nil
 	}
 
-	// Get the Cluster resource with this namespace/name
 	cluster, err := c.clustersLister.Clusters(namespace).Get(name)
 	if err != nil {
 		// The Cluster resource may no longer exist, in which case we stop
 		// processing.
 		if errors.IsNotFound(err) {
-			runtime.HandleError(fmt.Errorf("cluster '%s' in work queue no longer exists", key))
+			runtime.HandleError(fmt.Errorf("cluster '%s:%s' in work queue no longer exists", namespace, name))
 			return nil
 		}
 
 		return err
 	}
 
-	// TODO: list px objects in the cluster. Create them if required
-
-	// If an error occurs during Get/Create, we'll requeue the item so we can
-	// attempt processing again later. This could have been caused by a
-	// temporary network failure, or any other transient reason.
-	/*if err != nil {
-		return err
-	}*/
-
-	// If the Deployment is not controlled by this Foo resource, we should log
-	// a warning to the event recorder and ret
-	/*if !metav1.IsControlledBy(deployment, foo) {
-		msg := fmt.Sprintf(MessageResourceExists, deployment.Name)
-		c.recorder.Event(foo, corev1.EventTypeWarning, ErrResourceExists, msg)
-		return fmt.Errorf(msg)
-	}*/
-
-	// TODO Do whatever you need to sync the cluster state
+	switch ev.eventType {
+	case kwatch.Added:
+		err = c.clusterProvider.Create(cluster)
+	case kwatch.Modified:
+		err = c.clusterProvider.Upgrade(cluster)
+	case kwatch.Deleted:
+		err = c.clusterProvider.Destroy(cluster)
+	default:
+		err = fmt.Errorf("unsupported event type for cluster: %s", cluster.Name)
+	}
 
 	// If an error occurs during Update, we'll requeue the item so we can
-	// attempt processing again later. THis could have been caused by a
+	// attempt processing again later. This could have been caused by a
 	// temporary network failure, or any other transient reason.
-	/*if err != nil {
+	if err != nil {
 		return err
-	}*/
+	}
 
+	// TODO implemented the update of the 'cluster' object here
 	// Finally, we update the status block of the Cluster resource to reflect the
 	// current state of the world
-	err = c.updateClusterStatus(cluster, nil)
+	err = c.updateClusterStatus(cluster)
 	if err != nil {
 		return err
 	}
@@ -306,7 +358,7 @@ func (c *Controller) syncHandler(key string) error {
 }
 
 // TODO fix the signature based on px objects
-func (c *Controller) updateClusterStatus(cluster *api.Cluster, deployment *appsv1beta2.Deployment) error {
+func (c *Controller) updateClusterStatus(cluster *api.Cluster) error {
 	// NEVER modify objects from the store. It's a read-only, local cache.
 	// You can use DeepCopy() to make a deep copy of original object and modify this copy
 	// Or create a copy manually for better performance
@@ -318,57 +370,11 @@ func (c *Controller) updateClusterStatus(cluster *api.Cluster, deployment *appsv
 	return err
 }
 
-// enqueueCluster takes a Cluster resource and converts it into a namespace/name
-// string which is then put onto the work queue. This method should *not* be
-// passed resources of any type other than Cluster.
-func (c *Controller) enqueueCluster(obj interface{}) {
-	var key string
-	var err error
-	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
-		runtime.HandleError(err)
-		return
-	}
-	c.workqueue.AddRateLimited(key)
-}
-
-// handleObject will take any resource implementing metav1.Object and attempt
-// to find the Cluster resource that 'owns' it. It does this by looking at the
-// objects metadata.ownerReferences field for an appropriate OwnerReference.
-// It then enqueues that Cluster resource to be processed. If the object does not
-// have an appropriate OwnerReference, it will simply be skipped.
-func (c *Controller) handleObject(obj interface{}) {
-	var object metav1.Object
-	var ok bool
-	if object, ok = obj.(metav1.Object); !ok {
-		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
-		if !ok {
-			runtime.HandleError(fmt.Errorf("error decoding object, invalid type"))
-			return
-		}
-		object, ok = tombstone.Obj.(metav1.Object)
-		if !ok {
-			runtime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
-			return
-		}
-		logrus.Infof("Recovered deleted object '%s' from tombstone", object.GetName())
-	}
-
-	logrus.Infof("Processing object: %s", object.GetName())
-	if ownerRef := metav1.GetControllerOf(object); ownerRef != nil {
-		// If this object is not owned by a Cluster, we should not do anything more
-		// with it.
-		if ownerRef.Kind != "Cluster" {
-			logrus.Infof("[debug] Ignoring object %s since we don't own it", ownerRef.Name)
-			return
-		}
-
-		cluster, err := c.clustersLister.Clusters(object.GetNamespace()).Get(ownerRef.Name)
-		if err != nil {
-			logrus.Infof("ignoring orphaned object '%s' of cluster '%s'", object.GetSelfLink(), ownerRef.Name)
-			return
-		}
-
-		c.enqueueCluster(cluster)
-		return
-	}
+// CreateRecorder creates a event recorder
+func CreateRecorder(kubecli kubernetes.Interface, name, namespace string) record.EventRecorder {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(logrus.Infof)
+	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{
+		Interface: v1core.New(kubecli.Core().RESTClient()).Events(namespace)})
+	return eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: name})
 }
